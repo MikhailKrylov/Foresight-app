5f461a10ba4b998e21e7a6dc6fdec1c7dcbda8db
MikhailKrylov <MikhailKrylov@2011.auditory.ru>
2012-07-16 15:53:50 +0400

    Она рисует линии)

--- FSA_base/FSA_interface.py
+++ FSA_base/FSA_interface.py
@@ -25,14 +25,26 @@ class fsainterface(object):
             "quit" : self.quit,
         }
         self.wTree.signal_autoconnect( dic )
+        self.area = self.wTree.get_widget("MainDrawingArea")
         hruler1 = self.wTree.get_widget("hruler1")
         def motion_notify(ruler, event):
             return ruler.emit("motion_notify_event", event)
-        self.wTree.get_widget("MainDrawingArea").connect_object("motion_notify_event", motion_notify, hruler1)
+        self.area.connect_object("motion_notify_event", motion_notify, hruler1)
+        # self.drawing_(coord)
+        def mouseclick(Empty_arg,event = None):
+            coord = (0,0, int(event.x), int(event.y))
+            self.drawing_(coord)
+            
+        self.area.connect_object("button_press_event", mouseclick, None)   
         gtk.main()
     def quit(self, widget):
         sys.exit(0)
-
+    def drawing_(self, coord):
+        x1, y1, x2, y2 = coord
+        style = self.area.get_style()
+        gc = style.fg_gc[gtk.STATE_NORMAL]
+        drawable = self.area.window
+        drawable.draw_line(gc, x1, y1, x2, y2)
 
 aa = fsainterface()
         
