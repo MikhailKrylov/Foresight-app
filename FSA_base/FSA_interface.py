c7dc6b21a216749f36d99331e3b3bac225f66caf
MikhailKrylov <MikhailKrylov>
2012-07-18 12:22:53 +0400

    Кординально изменен подход к окну выбора шрифта. Текст пишется, но шрифт пока не меняется.

--- FSA_base/FSA_interface.py
+++ FSA_base/FSA_interface.py
@@ -23,65 +23,49 @@ except:
 class Font_selection_window(object):
     wTree = None
     def __init__(self, parent):
-        self.wTree = gtk.glade.XML( "Font_Selection_interface.glade" ) #подключение glade оболочки
-        dic = { 
-            "quit" : self.quit,
-            "Ok_btn_clicked" : self.font_windows_OK,
-            "Close_btn_clicked": self.quit
-        }
-        self.wTree.signal_autoconnect(dic)
+        self.wTree = gtk.glade.XML( "Font_Selection_interface2.glade" ) #подключение glade оболочки
         self.window = self.FontWindow = self.wTree.get_widget("FontWindow")
-        self.window
+        self.cansel_btn = self.wTree.get_widget("cancel_btn")
+        self.ok_btn = self.wTree.get_widget("ok_btn")
         self.parent = parent
         self.fontseldlg = self.wTree.get_widget("fontselection1")
+        def quit_(Emty_arg): #выход
+            self.window.destroy()
+        self.cansel_btn.connect_object("clicked", quit_, None) #обработка нажатия клавиши "Закрыть"
+        def OK_(Emty_arg): #Применить и выйти
+
+            self.parent.font = self.window.get_font_name()
+
+            self.window.destroy()
+        self.ok_btn.connect_object("clicked", OK_, None) #Обработка нажатия клавиши "Ок"
         gtk.main()
-    def quit(self, widget): #выход
-        sys.getrefcount(self)
-    def font_windows_OK(self,widget): 
-        print "AAA"
-        self.parent.font_name = self.fontseldlg.get_font_name()
-        sys.getrefcount(self)
-    
+        
     
 class fsainterface(object):
     wTree = None
     def __init__(self):
         self.wTree = gtk.glade.XML( "main_interface.glade" ) #подключение glade оболочки
-        dic = {     
-            "quit" : self.quit,
-            "open_font_dialog": self.open_font_dialog
-            
-        }
-        self.FontWindow = self.wTree.get_widget("FontWindow")
-        #self.FontWindow.show()
-        #self.point = (-1,-1)
-        self.wTree.signal_autoconnect(dic)
+        self.point = (-1,-1)
         self.area = self.wTree.get_widget("MainDrawingArea")
-        self.font_name = None
+        self.font = "Sans 19"
         self.font_sel_btn = self.wTree.get_widget("font_select_btn")
-        #self.fontseldlg.show()
         hruler1 = self.wTree.get_widget("hruler1")
         def motion_notify(ruler, event): #обработка движения мыши по зоне рисования
             return ruler.emit("motion_notify_event", event)
         self.area.connect_object("motion_notify_event", motion_notify, hruler1)
         def mouseclick(Empty_arg,event = None): #обработка щелчка мыши по зоне рисования
             coord = (int(event.x), int(event.y),0,0)
-            #self.draw_text(coord)
-            
+            self.draw_text(coord)
+            '''
             if self.point == (-1, -1):
                 self.point = (int(event.x), int(event.y))
             else:
                 coord = (self.point[0],self.point[1], int(event.x), int(event.y))
                 self.drawing_(coord)
                 self.point = (-1,-1)
-            
+            '''
         self.area.connect_object("button_press_event", mouseclick, None)  
-        self.font_sel_btn.connect_object("button_press_event", self.open_font_dialog, self.FontWindow) 
-        def font_windows_OK(): #Обработка нажатия клавиши ОК на форме выбора шрифта
-            self.FontWindow.hide()
-       # self.wTree.get_widget("Font_d_Ok_btn").connect_object(button_press_event)
-        def font_windows_Close(link_, event): #Обработка нажатия клавиши Отмена на форме выбора шрифта
-            pass         
+        self.font_sel_btn.connect("button_press_event", self.open_font_dialog) 
         gtk.main()
     def open_font_dialog(self, widget, ttt):
         fsw = Font_selection_window(self) 
@@ -100,11 +84,11 @@ class fsainterface(object):
     def draw_text(self, coord):
         x1, y1, x2, y2 = coord 
         drawable = self.area.window
-        color = drawable.get_colormap().alloc(random.randint(0,65535), random.randint(0,65535), random.randint(0,65535))
+        #color = drawable.get_colormap().alloc(random.randint(0,65535), random.randint(0,65535), random.randint(0,65535))
         gc = drawable.new_gc() 
-        gc.foreground = color
-        print gtk.Style
-        drawable.draw_string = (font, gc, x1, y1 , "Hello, World!")
+        #gc.foreground = color
+        font = gtk.gdk.Font(self.font)
+        drawable.draw_text(font, gc, x1, y1 , str(self.font))
     
         
 aa = fsainterface()
