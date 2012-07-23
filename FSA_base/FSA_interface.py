0c3490cd5c0401287e78bcc0403c325029b3b756
MikhailKrylov <MikhailKrylov@2011.auditory.ru>
2012-07-24 00:57:49 +0400

    Рисует стрелку)

--- FSA_base/FSA_interface.py
+++ FSA_base/FSA_interface.py
@@ -6,7 +6,7 @@ Created on 16.07.2012
 '''
 
 #подключение библиотек
-import sys, random, pango
+import sys, random, pango, time
 try:  
     import pygtk  
     pygtk.require("2.0")  
@@ -19,6 +19,44 @@ except:
     print("GTK Not Availible")
     sys.exit(1)
 import db_interface #подключение модуля работы с базой данных
+class trend_class(object): 
+    def __init__(self, dw_area, font, parameters = 0):
+        self.area = dw_area
+        self.power = 2
+        self.font = font
+        self.color = dw_area.window.get_colormap().alloc(random.randint(0,65535), random.randint(0,65535), random.randint(0,65535))
+        self.f_time = 2010
+        self.l_time = 2050
+        self.f_point = int(float(self.area.allocation.width)/55. * (self.f_time - 2000.))
+        self.l_piont = int(float(self.area.allocation.width)/55. * (self.l_time - 2000.))
+        self.text = u"Эколофт"
+        self.y = 300
+        self.text_rnd = True
+        self.arrow_rnd = True
+    def rendring(self):
+        x1 = self.f_point
+        x2 = self.l_piont 
+        drawable = self.area.window
+        #gc Graphics Context, т.е. параметры.
+        gc = drawable.new_gc() 
+        #цвет задается в интервале от 0 до 65535
+        gc.foreground = self.color
+        gc.line_width = self.power*2
+        drawable.draw_line(gc, x1, self.y, x2, self.y)
+        if self.text_rnd == True:
+            self.render_text()
+        if self.arrow_rnd == True:
+            drawable.draw_line(gc, x2-self.power*5, self.y-self.power*5, x2, self.y+1)
+            drawable.draw_line(gc, x2-self.power*5, self.y+self.power*5, x2, self.y-1)
+    def render_text(self):
+        x1 = self.f_point
+        drawable = self.area.window
+        gc = drawable.new_gc() 
+        gc.foreground = self.color
+        layout = self.area.create_pango_layout(self.text)
+        layout.set_font_description(pango.FontDescription(self.font))
+        font_size = int(self.font.split(" ")[1])*2
+        drawable.draw_layout(gc, x1, self.y-font_size, layout)
 #главный класс для работы с графическим интерфейсом
 class New_trend_dialog(object): #класс описывающий диалог внесения в базу нового тренда или редактирования существующего
     wTree = None
@@ -62,6 +100,7 @@ class fsainterface(object):
         self.point = (-1,-1)
         self.area = self.wTree.get_widget("MainDrawingArea")
         self.font = "Sans 19"
+        #self.area.allocation.height
         self.font_sel_btn = self.wTree.get_widget("font_select_btn")
         self.new_btn = self.wTree.get_widget("new_trend_btn")
         hruler1 = self.wTree.get_widget("hruler1")
@@ -76,8 +115,8 @@ class fsainterface(object):
                 self.point = (int(event.x), int(event.y))
             else:
                 coord = (self.point[0],self.point[1], int(event.x), int(event.y))
-                self.drawing_(coord)
-                self.point = (-1,-1)
+                #self.drawing_line(coord)
+                aa = trend_class(self.area, self.font)
 
         self.area.connect_object("button_press_event", mouseclick, None)  
         self.font_sel_btn.connect("button_press_event", self.open_font_dialog)
@@ -88,25 +127,18 @@ class fsainterface(object):
         ntw = New_trend_dialog(self)
     def quit(self, widget): #выход (уничтожение окна)
         self.wTree.get_widget("MainWindow").destroy()
-    def drawing_(self, coord):
+    def drawing_line(self, coord, color):
         x1, y1, x2, y2 = coord 
         drawable = self.area.window
         #gc Graphics Context, т.е. параметры.
         gc = drawable.new_gc() 
         #цвет задается в интервале от 0 до 65535
         color = drawable.get_colormap().alloc(random.randint(0,65535), random.randint(0,65535), random.randint(0,65535))        
-        gc.foreground =color
+        gc.foreground = color
         gc.line_width = random.randint(1,10)
         drawable.draw_line(gc, x1, y1, x2, y2)
     def draw_text(self, coord, text):  #Функция отображения текса в Drawing Area
-        x1, y1, x2, y2 = coord 
-        drawable = self.area.window
-        color = drawable.get_colormap().alloc(random.randint(0,65535), random.randint(0,65535), random.randint(0,65535))
-        gc = drawable.new_gc() 
-        gc.foreground = color
-        layout = self.area.create_pango_layout(text)
-        layout.set_font_description(pango.FontDescription(self.font))
-        drawable.draw_layout(gc, x1, y1, layout)
+      pass
     def db_visual(self,coord): #обращение к интерфейсу базы данных
         type_string = "trend_name UTF8(100), comment UTF8(300),  sources TEXT(300), rsh TEXT(300), s_point INTEGER(32), f_point INTEGER(32)"
         trend_base = db_interface.base(type_string)
@@ -124,4 +156,3 @@ class fsainterface(object):
         drstr = trend_base.print_db()
         self.draw_text(coord, drstr)
 aa = fsainterface()
-        
